d9b5a25f160ace7b5a47ba6fcf947587ffdef5d1
Tianhao Huang <tianhao.alex.huang@gmail.com>
2025-01-12 23:17:40 -0700

    init
    Signed-off-by: Tianhao Huang <tianhao.alex.huang@gmail.com>

---
+++ cdvae/utils.py
@@ -0,0 +1,45 @@
+import hydra
+from pathlib import Path
+from typing import List
+from hydra.core.hydra_config import HydraConfig
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning import seed_everything, Callback
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor, ModelCheckpoint
+
+
+def build_callbacks(cfg: DictConfig) -> List[Callback]:
+    callbacks: List[Callback] = []
+
+    if "lr_monitor" in cfg.logging:
+        hydra.utils.log.info("Adding callback <LearningRateMonitor>")
+        callbacks.append(
+            LearningRateMonitor(
+                logging_interval=cfg.logging.lr_monitor.logging_interval,
+                log_momentum=cfg.logging.lr_monitor.log_momentum,
+            )
+        )
+
+    if "early_stopping" in cfg.train:
+        hydra.utils.log.info("Adding callback <EarlyStopping>")
+        callbacks.append(
+            EarlyStopping(
+                monitor=cfg.train.monitor_metric,
+                mode=cfg.train.monitor_metric_mode,
+                patience=cfg.train.early_stopping.patience,
+                verbose=cfg.train.early_stopping.verbose,
+            )
+        )
+
+    if "model_checkpoints" in cfg.train:
+        hydra.utils.log.info("Adding callback <ModelCheckpoint>")
+        callbacks.append(
+            ModelCheckpoint(
+                dirpath=Path(HydraConfig.get().run.dir),
+                monitor=cfg.train.monitor_metric,
+                mode=cfg.train.monitor_metric_mode,
+                save_top_k=cfg.train.model_checkpoints.save_top_k,
+                verbose=cfg.train.model_checkpoints.verbose,
+            )
+        )
+
+    return callbacks
